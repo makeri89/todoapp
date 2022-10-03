8ff60004f3a9beca1fd0f5d7cc5c00dc37a19d66
Markus Kaihola <markus.kaihola@helsinki.fi>
2022-10-03 16:18:39 +0300

    Removed Auth0Provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,17 +1,11 @@
 import NextAuth from 'next-auth'
 import type { NextAuthOptions } from 'next-auth'
-import Auth0Provider from 'next-auth/providers/auth0'
 import GoogleProvider from 'next-auth/providers/google'
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import clientPromise from '@lib/mongodb'
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID!,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET!,
-      issuer: process.env.AUTH0_ISSUER,
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
